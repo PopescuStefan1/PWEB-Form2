1c28f9ce20811596429db7912f5d7ea3f22a5e4c
Stefro <stefan.ro95@gmail.com>
2023-03-20 20:25:26 +0200

    Add validation for email

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function validateForm() {
     let pNume = document.forms["myForm"]["pnume"].value;
-    if(pNume.length<2){
+    if (pNume.length < 2) {
         alert("Prenumele trebuie sa contina cel putin 2 litere");
         return false;
     }
 
     let nume = document.forms["myForm"]["nume"].value;
-    if(nume.length<2){
+    if (nume.length < 2) {
         alert("Numele trebuie sa contina cel putin 2 litere");
+        return false;
+    }
+
+    let email = document.forms["myForm"]["email"].value;
+    if (!(email.includes("@")) || !(email.includes("."))) {
+        alert("Email-ul trebuie sa contina @ si sa se termine in .ceva");
         return false;
     }
 }
