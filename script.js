45696282d020e4e6a340305450c6512b1ca3a573
PopescuStefan1 <stefan.ro95@gmail.com>
2023-04-12 21:02:36 +0300

    Change javascript to jquery

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function validateForm() {
-    let pNume = document.forms["myForm"]["pnume"].value;
+    let pNume = $("form[name='myForm'] input[name='pnume']").val();
     if (pNume.length < 2) {
         alert("Prenumele trebuie sa contina cel putin 2 litere");
         return false;
     }
 
-    let nume = document.forms["myForm"]["nume"].value;
+    let nume = $("form[name='myForm'] input[name='nume']").val();
     if (nume.length < 2) {
         alert("Numele trebuie sa contina cel putin 2 litere");
         return false;
     }
 
-    let email = document.forms["myForm"]["email"].value;
+    let email = $("form[name='myForm'] input[name='email']").val();
     if (!(email.includes("@")) || !(email.includes("."))) {
         alert("Email-ul trebuie sa contina @ si sa se termine in .ceva");
         return false;
     }
 
-    let nrTelefon = document.forms["myForm"]["telefon"].value;
+    let nrTelefon = $("form[name='myForm'] input[name='telefon']").val();
     if (nrTelefon.length > 10) {
         alert("Numarul de telefon introdus este prea lung");
         return false;
     }
 
-    let motivation = document.forms["myForm"]["motivatie"].value;
+    let motivation = $("form[name='myForm'] textarea[name='motivatie']").val();
     if (motivation.length < 100) {
         alert("Motivatia este prea scurta");
         return false;
